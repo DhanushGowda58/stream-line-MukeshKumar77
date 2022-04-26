4a488266df8f01dfc5511420d1c51a4b673f7256
DhanushGowda58 <90901324+DhanushGowda58@users.noreply.github.com>
2022-04-26 11:16:21 +0530

    Create BMS_Receiver.c

---
+++ BMS_Receiver.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#define NUM_OF_READINGS 50
+void main()
+{
+  int RxdSig[NUM_OF_READINGS];
+  for (int i=0; i<NUM_OF_READINGS; i++)
+  {
+    scanf("%d\n",&RxdSig);
+  }
+}
