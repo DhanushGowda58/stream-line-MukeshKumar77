35df9959a05e2a127084f2ef2dab68ef837119eb
DhanushGowda58 <90901324+DhanushGowda58@users.noreply.github.com>
2022-04-26 12:00:18 +0530

    Update BMS_Receiver.c

--- BMS_Receiver.c
+++ BMS_Receiver.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include "BMS_sender.h"
 
-int main()
+bool BMS_DataReceiver()
 {
   int RxdSig[NUMBERS_OF_READINGS];
   for (int i=0; i<NUMBERS_OF_READINGS; i++)
@@ -10,5 +11,5 @@ int main()
     scanf("%d\n",&RxdSig[i]);
     printf("Temperature Data = %d\n", TemperatureData[i]);
   }
-  return 0;
+  return true;
 }
