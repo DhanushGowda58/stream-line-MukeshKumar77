0d458d128b8bfca021197ceeab3a48a3d55d1b95
DhanushGowda58 <90901324+DhanushGowda58@users.noreply.github.com>
2022-04-26 12:12:17 +0530

    Update BMS_Receiver.c

--- BMS_Receiver.c
+++ BMS_Receiver.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 //#include <stdbool.h>
 #include "BMS_sender.h"
-int batteryData[NUMBERS_OF_READINGSi];
-int main()
+void printToConsole(int batteryData[]);
 {
   
   int RxdSig[NUMBERS_OF_READINGS];
@@ -12,5 +11,4 @@ int main()
     scanf("%d\n",&RxdSig[i]);
     printf("Temperature Data = %d\n", batteryData[i]);
   }
-  return 0;
 }
