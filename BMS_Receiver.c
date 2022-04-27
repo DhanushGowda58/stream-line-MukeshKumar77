ce27e38754402a86a39b5dc7627d5594497717de
DhanushGowda58 <90901324+DhanushGowda58@users.noreply.github.com>
2022-04-27 20:36:03 +0530

    Update BMS_Receiver.c

--- BMS_Receiver.c
+++ BMS_Receiver.c
@@ -6,12 +6,12 @@ int main()
   int TempData[100];
    long int i;
   int min = 0, max = 0;
-   for( i=0; i<100; i++ ) 
+   for( i=0; i<100; i+2 ) 
    {
     TempData[i] = getc(stdin); 
     putc(TempData[i],stdout);
    } 
-   for( i=23; i<100; i++ ) 
+   for( i=23; i<100; i+2 ) 
    {       
     min = max = TempData[0];
 	if (min > TempData[i])
