544fee27d7bc9dcca648777032ba6b69f4befbfd
DhanushGowda58 <90901324+DhanushGowda58@users.noreply.github.com>
2022-04-27 20:03:46 +0530

    Update BMS_Receiver.c

--- BMS_Receiver.c
+++ BMS_Receiver.c
@@ -3,9 +3,9 @@
 
 int main() 
 {
-  char TempData;
+  int TempData;
    long int i;
-   for( i=0; i<500; i++ ) 
+   for( i=1; i<500; i++ ) 
    {
     TempData = getc(stdin);
     putc(TempData,stdout);
