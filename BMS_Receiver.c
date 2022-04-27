62de8db1a3dbc12bbb8da2af308962b2f1b0ec31
DhanushGowda58 <90901324+DhanushGowda58@users.noreply.github.com>
2022-04-27 23:57:35 +0530

    Update BMS_Receiver.c

--- BMS_Receiver.c
+++ BMS_Receiver.c
@@ -3,16 +3,17 @@
 
 int main() 
 {
-  char TempData[500] = {0};
+  string TempData;
    long int i,j,k;
-  int min = 0, max = 0;
+  //int min = 0, max = 0;
 
    for( i=0; i<500; i++ ) 
    {
-    TempData[i] = getc(stdin);
+    TempData= getc(stdin);
+	   printf("%s\n", TempData);
    } 
-  min = max = TempData[0];
-   for( j=0; j<500; j++ ) 
+  /*min = max = TempData[0];
+  for( j=0; j<500; j++ ) 
    {
 	if (min > TempData[j])
 		min = TempData[j];
@@ -25,5 +26,5 @@ int main()
    for( k=0; k<500; k++ ) 
    {
 	printf("%c\n", TempData[k]);
-   }
+   }*/
 }
