88eb78a6e63b24d943a4d7febe84db592de2c2e9
DhanushGowda58 <90901324+DhanushGowda58@users.noreply.github.com>
2022-04-27 19:59:22 +0530

    Update BMS_Receiver.c

--- BMS_Receiver.c
+++ BMS_Receiver.c
@@ -1,24 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#include <stdbool.h>
-#include "BMS_sender.h"
-/*void printToConsole(int batteryData[])
-{
-  int RxdSig[NUMBERS_OF_READINGS];
-  for (int i=0; i<NUMBERS_OF_READINGS; i++)
-  {
-    scanf("%d\n",&RxdSig[i]);
-    printf("Temperature Data = %d\n", batteryData[i]);
-  }
-}*/
 
 int main() 
 {
-  char c;
+  int TempData;
    long int i;
    for( i=0; i<500; i++ ) 
    {
-    c=getc(stdin);
-    putc(c,stdout);
+    TempData = getc(stdin);
+    putc(TempData,stdout);
    }       
 }
