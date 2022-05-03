5c0910c560a6a31c821626e3fc2491027855160f
DhanushGowda58 <90901324+DhanushGowda58@users.noreply.github.com>
2022-05-03 13:26:59 +0530

    Update BMS_Receiver.c

--- BMS_Receiver.c
+++ BMS_Receiver.c
@@ -6,7 +6,7 @@ char TempData[600];
 static void findMinMax()
 {
 	long int j = 0;
-  	char min, max;
+  	int min, max;
 	  min = max = TempData[24];
 	  for( j=24; j<165; j++ ) 
 	   {
@@ -16,8 +16,8 @@ static void findMinMax()
 			max = TempData[j];
 	    //putc(TempData[j],stdout);
 	   }  
-	  printf("\n Minimum value = %c\n", min);
-	  printf("Maximum value = %c\n", max);
+	  printf("\n Minimum value = %d\n", min);
+	  printf("Maximum value = %d\n", max);
 }
 
 int main() 
@@ -26,8 +26,7 @@ long int i =0;
    for( i=0; i<600; i++ ) 
    {
 	TempData[i] = getc(stdin);
-	 printf("%c - %d", TempData[i],TempData[i]);
-
+	printf("%c", TempData[i]);
    } 
 
 printf("Computing Min and Max\n");
