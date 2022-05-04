aac1ff0a9d2562498e27e5bd06b83a46c09fe1e3
DhanushGowda58 <90901324+DhanushGowda58@users.noreply.github.com>
2022-05-04 18:00:24 +0530

    Update Receiver_test.cpp

--- Receiver_test.cpp
+++ Receiver_test.cpp
@@ -17,6 +17,14 @@ TEST_CASE("Computing MinMax")
     REQUIRE(findMinMax(Data, entity, unit, 10) == 80);
 }
 
+TEST_CASE("Computing Average")
+{
+    int Data[10] = {20,10,30,5,0,50,60,80,25,12};
+    char entity[] = "Temperature";
+    char unit[] = "degC - TESTCASE";
+    REQUIRE(Avg(Data, entity, unit, 10) == float (29.2));
+}
+
 TEST_CASE("Simple Moving Average -> Success Case")
 {
     int Data[10] = {20,10,30,5,0,50,60,80,25,12};
