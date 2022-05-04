b0c0c9a50b65e96ca9cca772b728014dc0dc77f3
DhanushGowda58 <90901324+DhanushGowda58@users.noreply.github.com>
2022-05-04 12:48:18 +0530

    Create Receiver_test.cpp

---
+++ Receiver_test.cpp
@@ -0,0 +1,12 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#include "test/catch.hpp"
+#include "BMS_Receiver.h"
+
+FILE* fptr;
+int sensorData[NUMBERS_OF_READINGS];
+
+TEST_CASE("Retrieving data to console")
+{
+    REQUIRE(BMS_Receiver() == true);
+}
