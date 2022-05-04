6200d98b5a9ca5934379432abff208934649d88f
DhanushGowda58 <90901324+DhanushGowda58@users.noreply.github.com>
2022-05-04 21:26:19 +0530

    Create BMS_Receiver.h

---
+++ BMS_Receiver.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+#define NUMBERS_OF_READINGS 50
+int BMS_Receiver(void); 
+int findMinMax(int Data[], char *entity, char *unit, int NoOfReadings);
+float Avg(int Data[], char *entity, char *unit, int NoOfReadings);
+float SimMovAvg(int Data[], char *entity, char *unit, int NoOfReadings);
